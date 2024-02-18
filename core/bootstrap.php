327bb58feb63bb73b0895fd1a9cae9fe7930eb94
Ankit-Upadhyay-73 <ank7559@gmail.com>
2024-02-18 10:39:24 +0530

    rate-limiter added

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+
+    session_start();
+
     require 'App.php';
     require 'public/helper.php';
     App::bind('config',require 'config.php');
@@ -10,11 +13,30 @@
     require 'controllers/AuthController.php';
     require 'controllers/ProductController.php';
     require 'controllers/HomeController.php';
+
+    require 'RateLimiter.php';
+
+    $rateLimiter = new RateLimiter($_SERVER["REMOTE_ADDR"]);
+
+    $limit = 1;				//	number of connections to limit user to per $minutes
+    $minutes = 1;				//	number of $minutes to check for.
+    $seconds = floor($minutes * 60);	//	retry after $minutes in seconds.
+    
+    try {
+        $rateLimiter->limitRequestsInMinutes($limit, $minutes);
+    } catch (RateExceededException $e) {
+        header("HTTP/1.1 429 Too Many Requests");
+        header(sprintf("Retry-After: %d", $seconds));
+        $data = 'Rate Limit Exceeded ';
+        die (json_encode($data));
+    }
+
     App::bind('database',
         new QueryBuilder(
             Connection::make(App::get('config')['database'])
         )
     );
+
     echo Router::load("routes.php")
             ->direct(Request::uri(),Request::method());
 
